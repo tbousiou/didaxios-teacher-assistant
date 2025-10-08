aaab9d2b983904fa183fe2992747b5c0f3a4f2af
Teo Bous <tbousiou@gmail.com>
2025-10-08 12:41:34 +0300

    Setup nativewind

--- app/(maintabs)/index.tsx
+++ app/(maintabs)/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 
+
 export default function HomeScreen() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home Screen</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to Nativewind!
+            </Text>
+        </View>
+    );
 }
